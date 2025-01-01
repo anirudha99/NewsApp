8f7b11cb7d967ebe0172ac3f621b0bd7fa6aedbc
Anirudha SM <anirudha.sm@ymedialabs.com>
2025-01-01 11:18:38 +0530

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Anirudha SM on 01/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
