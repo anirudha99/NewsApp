f88f14b939a68b3a6c941abbf8021e5948090c6e
Anirudha SM <anirudha.sm@ymedialabs.com>
2025-01-01 20:54:01 +0530

    added unit test case for ArticleDetailViewModel

--- NewsAppTests/NewsAppTests.swift
+++ NewsAppTests/NewsAppTests.swift
@@ -8,29 +8,73 @@
 import XCTest
 @testable import NewsApp
 
-final class NewsAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class MockNetworkService: NetworkServiceProtocol {
+    var shouldReturnError: Bool = false
+    
+    func fetchArticleDetails(articleId: String) async throws -> ArticleDetails {
+        if shouldReturnError {
+            throw URLError(.badServerResponse)
         }
+        return ArticleDetails(likes: 100, comments: 50)  // Simulate a successful response
     }
 
+    func fetchNews() async throws -> [Article] {
+        if shouldReturnError {
+            throw URLError(.notConnectedToInternet)
+        }
+        return [
+            Article(title: "Title 1", description: "Description 1", author: "Author 1", urlToImage: nil, url: "http://example.com/1"),
+            Article(title: "Title 2", description: "Description 2", author: "Author 2", urlToImage: nil, url: "http://example.com/2")
+        ]
+    }
+}
+
+final class NewsAppTests: XCTestCase {
+    
+    /// Unit Test cases for ArticleDetailViewModel
+    
+    var viewModel: ArticleDetailViewModel!
+    var mockNetworkService: MockNetworkService!
+    
+    override func setUp() {
+        super.setUp()
+        mockNetworkService = MockNetworkService()
+        let article = Article(title: "Sample Article", description: "Sample description", author: "Author", urlToImage: nil, url: "http://example.com")
+        viewModel = ArticleDetailViewModel(article: article, networkService: mockNetworkService)
+    }
+    
+    override func tearDown() {
+        viewModel = nil
+        mockNetworkService = nil
+        super.tearDown()
+    }
+    
+    // Test successful fetch
+    func testFetchDetailsSuccess() async {
+        // Given
+        mockNetworkService.shouldReturnError = false
+        
+        // When
+        await viewModel.fetchDetails()
+        
+        // Then
+        XCTAssertNotNil(viewModel.details)
+        XCTAssertEqual(viewModel.details?.likes, 100)
+        XCTAssertEqual(viewModel.details?.comments, 50)
+        XCTAssertNil(viewModel.error)
+    }
+    
+    // Test failed fetch with error
+    func testFetchDetailsFailure() async {
+        // Given
+        mockNetworkService.shouldReturnError = true
+        
+        // When
+        await viewModel.fetchDetails()
+        
+        // Then
+        XCTAssertNil(viewModel.details)
+        XCTAssertNotNil(viewModel.error)
+    }
+    
 }
